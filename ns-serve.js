b15eda6794dc942acb5f66813590c26f2b2fbb64
Meghashyam Bhandary <r21meghashyam@gmail.com>
2021-03-11 00:57:34 +0530

    Update ns-serve.js

--- ns-serve.js
+++ ns-serve.js
@@ -1,3 +1,2 @@
 #! /usr/bin/env node
-//require('http').createServer((r,s)=>require('fs').readFile(process.argv[1]+(u=r.url.split(/\?|\:|\#/)[0],u=="/"?"/index.html":u),(e,d)=>s.end(e?e.message: d))).listen(8080)
-console.log(require("path").resolve(""));
+require('http').createServer((r,s)=>require('fs').readFile(require("path").resolve("")+(u=r.url.split(/\?|\:|\#/)[0],u=="/"?"/index.html":u),(e,d)=>s.end(e?e.message: d))).listen(8080)
